3f576b9b57115b611f916fce799d18dd2e533833
Joseph Ellis <josephellis95@gmail.com>
2024-09-27 18:14:24 -0500

    final ticket touches

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -30,7 +30,6 @@ function RenderLandingPage(props) {
           </div>
         </div>
       </section>
-
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
       <section>
         <div className="graphs-section">
@@ -48,7 +47,7 @@ function RenderLandingPage(props) {
               alt="Human Rights First"
               className="graphpics"
             />
-            <p>Search Grant Rates By Office </p>
+            <p>Search Grant Rates By Nationality </p>
           </div>
           <div className="grant-rates-over-time-container">
             <img
@@ -56,7 +55,7 @@ function RenderLandingPage(props) {
               alt="Human Rights First"
               className="graphpics"
             />
-            <p>Search Grant Rates By Office </p>
+            <p>Search Grant Rates Over Time</p>
           </div>
         </div>
       </section>
@@ -111,16 +110,16 @@ function RenderLandingPage(props) {
         </div>
       </section>
 
-      <section className="subsection">
-        <div className="subsection_div">
+      <section className="SystemicDisparityInsights">
+        <div className="SystemicDisparityInsights_content">
           <h1>Systemic Disparity Insights</h1>
         </div>
       </section>
 
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        <section className="SystemicDisparityInsights">
-          <div className="moreLove">
+        <section className="subsection">
+          <div className="paragraphs">
             <h2>36%</h2>
             <p>
               By the end of the Trump administration, the average asylum office
@@ -128,14 +127,14 @@ function RenderLandingPage(props) {
               fiscal year 2016 to 28 percent in fiscal year 2020.
             </p>
           </div>
-          <div className="moreLove">
+          <div className="paragraphs">
             <h2>5%</h2>
             <p>
               The New York asylum office grant rate dropped to 5 percent in
               fiscal year 2020.
             </p>
           </div>
-          <div className="moreLove">
+          <div className="paragraphs">
             <h2>6x Lower</h2>
             <p>
               Between fiscal year 2017 and 2020, the New York asylum office’s
